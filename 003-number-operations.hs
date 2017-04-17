192bf7830cdacf69493e901078e55a03a645a1b3
Adhy <adhywiranatap@gmail.com>
2017-04-17 16:18:15 +0700

    updated number operations

--- 003-number-operations.hs
+++ 003-number-operations.hs
@@ -3,3 +3,9 @@ addByTwo x = print (x + 2)
 
 multiplyByThree :: Int -> IO ()
 multiplyByThree x = print (x * 3)
+
+subtractByFour :: Int -> IO ()
+subtractByFour x = print (x - 4)
+
+divideByFive :: Float -> IO ()
+divideByFive x = print (x / 5)
