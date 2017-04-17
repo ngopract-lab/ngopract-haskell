9dd20e6108006f265bc8951b21edddf0f146b5b1
Adhy <adhywiranatap@gmail.com>
2017-04-17 16:27:35 +0700

    added fraction operation

--- 003-number-operations.hs
+++ 003-number-operations.hs
@@ -7,5 +7,8 @@ multiplyByThree x = print (x * 3)
 subtractByFour :: Int -> IO ()
 subtractByFour x = print (x - 4)
 
-divideByFive :: Float -> IO ()
-divideByFive x = print (x / 5)
+divideByFive :: Int -> IO ()
+divideByFive x = print (div x 5)
+
+fractionBySix :: Float -> IO ()
+fractionBySix x = print (x / 6)
