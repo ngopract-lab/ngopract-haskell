6adad2f0e2c3ffd550ca9f6317b58b980769a9f5
Adhy <adhywiranatap@gmail.com>
2017-04-18 13:50:43 +0700

    added conditionals

---
+++ 018-if-else.hs
@@ -0,0 +1,4 @@
+justASimpleFunc x =
+  if(x > 1)
+    then "More than One"
+    else "Less than One or exactly One"
---
+++ 019-case-condition.hs
@@ -0,0 +1,10 @@
+justASimpleFunc x =
+  case x > 1 of
+    True -> "More than One"
+    False -> "Less or Equal to One"
+
+justAnotherSimpleFunc x =
+  case x of
+    1 -> "is One"
+    2 -> "is Two"
+    3 -> "is Three"
