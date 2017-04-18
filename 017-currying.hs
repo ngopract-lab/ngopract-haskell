a0e83d8f06edb5f8f25df5d0740328a6f3a3c056
Adhy <adhywiranatap@gmail.com>
2017-04-18 13:19:16 +0700

    added currying module

--- 017-currying.hs
+++ 017-currying.hs
@@ -1,3 +1,14 @@
+-- ABOUT CURRYING
+-- All functions in Haskell take one argument and return one
+-- result. Other programming languages, if you have any experience
+-- with them, typically allow you to define functions that can take multiple
+-- arguments.
+
+-- Uncurried functions: One function, many arguments
+-- Curried functions: Many functions, one argument apiece
+
+-- curry, will return an unary function (unary = 1-ary / 1-arity / single argument)
+
 module SimpleCurry where
   addition :: Int -> Int -> Int
   addition x y = x + y
