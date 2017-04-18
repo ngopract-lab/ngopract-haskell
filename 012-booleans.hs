0e64f6fd7af5aba1595de25ae8148bde5fe61242
Adhy <adhywiranatap@gmail.com>
2017-04-18 09:30:18 +0700

    added boolean operations reference

---
+++ 012-booleans.hs
@@ -0,0 +1,14 @@
+setBool :: Bool -> IO ()
+setBool b = print(b)
+-- bool in Haskell is True or False
+
+checkName :: String -> IO ()
+checkName s =
+  print(s == "wow") -- prints either True or False
+
+biggerThanFive :: Int -> IO ()
+biggerThanFive x =
+  print(x > 5) -- prints either True or False
+
+notSomething :: Bool -> IO ()
+notSomething b = print(not b) --prints a negation of b
