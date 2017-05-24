c58edc40979940d5927426ee67d64fbf7f7aaf46
Adhy <adhywiranatap@gmail.com>
2017-05-24 20:34:07 +0700

    modify let where module 006

--- 006-let-and-where.hs
+++ 006-let-and-where.hs
@@ -1,7 +1,11 @@
 module FunctionWithWhereOrLet where
   -- method 1: using where
   printInc n = print plusTwo
-    where plusTwo = n + 2
+    where
+      plusTwo = n + 2
   -- method 2: using let ... in
-  printInc2 n = let plusTwo = n + 2
-    in print plusTwo
+  printInc2 n =
+    let
+      plusTwo = n + 2
+    in
+      print plusTwo
