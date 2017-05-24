eaa240ccc761944cc1d765dfebeb20d9e7cbcc0d
Adhy <adhywiranatap@gmail.com>
2017-05-24 20:42:05 +0700

    update lambda function module variable and params naming 008

--- 008-more-lambda-functions.hs
+++ 008-more-lambda-functions.hs
@@ -1,8 +1,11 @@
 module ExampleLambdaFunctions where
-  multiply x = (\x -> print (x * 2)) x
-  -- backslash is for anonymous function syntax and will evaluate using x argument
-  showHello s = (\s -> putStrLn ("hey " ++ s)) s
-  moreComplicated x =
-    let y = x + 5
+  multiply xParam = (\num -> print (num * 2)) xParam
+  -- backslash is for anonymous function syntax and will evaluate using xParam argument
+  -- xParam will be then provided as a lamda function parameter
+
+  showHello sParam = (\aString -> putStrLn ("hey " ++ aString)) sParam
+
+  moreComplicated xParam =
+    let y = xParam + 5
     in (\y -> print (y * 2)) y
     -- if moreComplicated 5, 5 + 5 = 10, print 10 * 2 = 20 <-
