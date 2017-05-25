aa9493166f81ee9402c64d3d901d4141189d1496
Adhy <adhywiranatap@gmail.com>
2017-05-25 11:16:33 +0700

    update map operation 026

--- 026-map-operation.hs
+++ 026-map-operation.hs
@@ -1,5 +1,4 @@
 -- map is a function to modify a List
-
 myList = [1,2,3,4,5]
 
 -- 01. mapping a List to increase their values with 1
@@ -11,3 +10,7 @@ multiplyByThree :: Int -> Int
 multiplyByThree num = num * 3
 
 myModifiedListUsingHoF = map multiplyByThree myList -- [3,6,9,12,15]
+
+-- 03. fmap is the same as map, but not only for List.
+-- fmap itself is actually a Functor, which will be reviewed later.
+myAddedByOneListUsingFmap = fmap (+1) myList -- [2,3,4,5,6]
