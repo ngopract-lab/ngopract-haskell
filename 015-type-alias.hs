2cf52786410680bd82f742ee95aeaef69a0fb945
Adhy <adhywiranatap@gmail.com>
2017-05-25 10:36:24 +0700

    update type alias module 015

--- 015-type-alias.hs
+++ 015-type-alias.hs
@@ -1,5 +1,14 @@
-type Name = String
--- type Name is an alias to String
+-- type aliases are basically aliases for data types.
 
+-- 01. type Name is an alias to String
+type Name = String
+
+-- example of using a type alias
 sayHiToPerson :: Name -> String
 sayHiToPerson name = (++) "Hello " name
+
+-- 02. type alias of a Tuple
+type Info = (String, Int)
+
+processUserInfoName :: Info -> IO()
+processUserInfoName userInfoParam = print(fst userInfoParam)
